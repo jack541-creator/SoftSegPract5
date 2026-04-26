7403f2138bde0fff801227feaed2255280a1dc10
vnaslund-19 <victor.naslund@yahoo.com>
2026-04-26 16:39:55 +0200

    Test listar_servicios

--- tests/gestor_credenciales/test_funcionales_gestor_credenciales.py
+++ tests/gestor_credenciales/test_funcionales_gestor_credenciales.py
@@ -18,8 +18,34 @@ class TestFuncionalesGestorCredenciales(unittest.TestCase):
         self.fail()
 
     def test_listar_servicios(self):
-        # Implementar según TDD
-        self.fail()
+        clave = "claveMaestraSegura123!"
+
+        servicios = self.gestor.listar_servicios(clave)
+        self.assertIsInstance(servicios, list)
+        self.assertIsNotNone(servicios)
+        self.assertEqual(servicios, [])
+
+        self.gestor.añadir_credencial(clave, "GitHub", "user1", "PasswordSegura123!")
+        self.gestor.añadir_credencial(clave, "Eduroam", "user2", "PasswordSegura123!")
+        self.gestor.añadir_credencial(clave, "GitHub", "user3", "PasswordSegura123!")
+
+        servicios = self.gestor.listar_servicios(clave)
+        self.assertCountEqual(servicios, ["GitHub", "Eduroam"])
+
+        self.gestor.eliminar_credencial(clave, "Eduroam", "user2")
+        servicios = self.gestor.listar_servicios(clave)
+        self.assertEqual(servicios, ["GitHub"])
+
+        # eliminar todo
+        self.gestor.eliminar_credencial(clave, "GitHub", "user1")
+        self.gestor.eliminar_credencial(clave, "GitHub", "user3")
+
+        servicios = self.gestor.listar_servicios(clave)
+        self.assertEqual(servicios, [])
+
+        # autenticación
+        with self.assertRaises(ErrorAutenticacion):
+            self.gestor.listar_servicios("claveIncorrecta")
 
 if __name__ == "__main__":
     unittest.main()
