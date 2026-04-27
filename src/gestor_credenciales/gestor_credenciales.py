1d7925302f8ffae0b077204fb555bc533691b9f7
Ismael <ismaesco06@gmail.com>
2026-04-27 17:49:57 +0200

    Tests verificación por correo

--- src/gestor_credenciales/gestor_credenciales.py
+++ src/gestor_credenciales/gestor_credenciales.py
@@ -1,6 +1,7 @@
 import unittest
 import hashlib
 import bcrypt
+import smtplib
 from icontract import require, ensure
 
 class ErrorPoliticaPassword(Exception):
@@ -13,6 +14,18 @@ class ErrorServicioNoEncontrado(Exception):
     pass
 
 class ErrorCredencialExistente(Exception):
+    pass
+
+class ErrorCorreoNoEstablecido(Exception):
+    pass
+
+class ErrorCodigoNoEstablecido(Exception):
+    pass
+
+class ErrorSinIntentosRestantes(Exception):
+    pass
+
+class ErrorUsuarioYaVerificado(Exception):
     pass
 
 class GestorCredenciales:
@@ -49,12 +62,12 @@ class GestorCredenciales:
     def listar_servicios(self, clave_maestra: str) -> list:
         """Lista todos los servicios almacenados."""
         pass
-    
+
     @ensure(lambda result: isinstance(result, list))
     def listar_usuarios(self, clave_maestra:str) -> list:
         """Lista todos los usuarios en la base de datos."""
         pass
-        
+
     def _hash_clave(self, clave: str) -> str:
         """Hashea una clave usando bcrypt."""
         return bcrypt.hashpw(clave.encode('utf-8'), bcrypt.gensalt())
@@ -63,3 +76,61 @@ class GestorCredenciales:
         """Verifica si una clave coincide con su hash."""
         return bcrypt.checkpw(clave.encode('utf-8'), clave_hashed.encode('utf-8'))
 
+    def obtener_correo(self, clave_maestra: str, servicio:str, usuario: str) -> str | None:
+        try:
+            correo = self._credenciales[servicio][usuario]["correo"]
+        except KeyError:
+            correo = None
+
+        return correo
+
+    def obtener_codigo2fa(self, clave_maestra: str, servicio:str, usuario: str) -> str | None:
+        try:
+            codigo_auth = self._credenciales[servicio][usuario]["codigo_auth"]
+        except KeyError:
+            codigo_auth = None
+
+        return codigo_auth
+
+    def obtener_intentos_restantes(self, clave_maestra: str, servicio:str, usuario: str) -> int | None:
+        try:
+            intentos = self._credenciales[servicio][usuario]["intentos_restantes"]
+        except KeyError:
+            intentos = None
+
+        return intentos
+
+    def comprobacion_verificacion_usuario(self, clave_maestra: str, servicio:str, usuario: str) -> bool:
+        try:
+            verificado = self._credenciales[servicio][usuario]["verificado"]
+        except KeyError:
+            verificado = False
+
+        return verificado
+
+    def establecer_correo(self, clave_maestra: str, servicio: str, usuario: str, correo: str) -> None:
+        """Establece el correo verificando que este es correcto"""""
+        pass
+
+    def crear_codigo2fa(self, clave_maestra: str, servicio: str, usuario: str) -> None:
+        """Crea el codigo de auntenticación aleatoriamente y establece los intentos_restantes en 3"""
+        """El código tiene que tener una longitud mínima de 6"""
+        pass
+
+    def enviar_correo2fa(self, clave_maestra: str, servicio: str, usuario: str) -> None:
+        """Envia un correo usando tls con el contenido"""
+        """Debe llamar a los errores ErrorCorreoNoEstablecido o ErrorCodigoNoEstablecido si falta el correo o el codigo respectivamente"""
+        pass
+
+    def verificar_usuario(self, clave_maestra: str, servicio: str, usuario: str, codigo: str) -> None:
+        """Intento de verificación con el código pasado por el usuario"""
+        """Al llamar a la función:"""
+        """Si el codigo introducido no coincide con el del usuario se le resta un intento"""
+        """Si el codigo introducido coincide entonces (_credenciales[servicio][usuario]["verificado"] = True) y se cambia el codigo de autenticación y los intentos por 'None'"""
+        """Si el usuario ya está verificado entonces salta la excepcion 'ErrorUsuarioYaVerificado'"""
+        """Si los intentos están a 0 entonces salta la excepcion 'ErrorSinIntentosRestantes'"""
+        """Si el codigo de autenticación es 'None' salta la excepción 'ErrorCodigoNoEstablecido'"""
+        pass
+
+gestor = GestorCredenciales("claveMaestraSegura123!")
+print(gestor._verificar_clave("clave", gestor._hash_clave("clave")))
---
+++ tests/gestor_credenciales/test_verificacion_por_correo.py
@@ -0,0 +1,150 @@
+import unittest
+import icontract
+from unittest.mock import patch
+from gestor_credenciales.gestor_credenciales import GestorCredenciales, ErrorCodigoNoEstablecido, ErrorSinIntentosRestantes, ErrorUsuarioYaVerificado , ErrorCorreoNoEstablecido
+
+
+class Test2FA(unittest.TestCase):
+
+    def setUp(self):
+        self.gestor = GestorCredenciales("claveMaestraSegura123!")
+        self.clave = "claveMaestraSegura123!"
+        self.gestor.añadir_credencial(self.clave, "Github", "user1", "Password123!")
+
+    def test_estalecer_correo(self):
+
+        correo = "prueba@gmail.com"
+
+        self.gestor.establecer_correo(self.clave, "Github", "user1", correo)
+
+        # Comprueba que se ha añadido el correo
+        self.assertEqual(self.gestor.obtener_correo(self.clave, "Github", "user1"), correo)
+
+    # Comprobación de que se ha validado el correo usando @require de icontract
+    def test_validacion_correo(self):
+
+        # Sin usuario
+        with self.assertRaises(icontract.ViolationError):
+            self.gestor.establecer_correo(self.clave, "Github", "user1", "@gmail.com")
+            
+        # Sin dominio
+        with self.assertRaises(icontract.ViolationError):
+            self.gestor.establecer_correo(self.clave, "Github", "user1", "abcdf.com")
+
+        # Sin extensión
+        with self.assertRaises(icontract.ViolationError):
+            self.gestor.establecer_correo(self.clave, "Github", "user1", "test@gmail")
+
+
+    def test_creacion_codigo(self):
+
+        self.gestor.crear_codigo2fa(self.clave, "Github", "user1")
+
+        # La longitud del código es al menos de 6 y los intentos se establecen en 3
+        self.assertGreaterEqual(len(self.gestor.obtener_codigo2fa(self.clave, "Github", "user1")), 6)
+        self.assertEqual(self.gestor.obtener_intentos_restantes(self.clave, "Github", "user1"), 3)
+
+
+    @patch("gestor_credenciales.gestor_credenciales.smtplib.SMTP")
+    def test_correo2FA(self, mock_smtp):
+        
+        correo = "prueba@gmail.com"
+        self.gestor.establecer_correo(self.clave, "Github", "user1", correo)
+        self.gestor.crear_codigo2fa(self.clave, "Github", "user1")
+
+        self.gestor.enviar_correo2fa(self.clave, "Github", "user1")
+
+        # Comprobación de la conexión SMTP
+        mock_smtp.assert_called_once_with("smtp.gmail.com", 587)
+
+        smtp_instance = mock_smtp.return_value
+
+        # Comprobación de TLS
+        smtp_instance.starttls.assert_called_once()
+
+        # Comprobación del login de la cuenta que envia el correo
+        smtp_instance.login.assert_called_once()
+
+        # 4. Se envía el mensaje
+        smtp_instance.sendmail.assert_called_once()
+
+        # 5. El mensaje contiene el código
+        args = smtp_instance.sendmail.call_args[0]
+        mensaje = args[2]
+
+        self.assertIn(self.gestor.obtener_codigo2fa(self.clave, "Github", "user1"), mensaje)
+
+        # 6. Se cierra conexión
+        smtp_instance.quit.assert_called_once()
+
+    @patch("gestor_credenciales.gestor_credenciales.smtplib.SMTP")
+    def test_correo_sin_establecer(self, mock_smtp):
+
+        self.gestor.crear_codigo2fa(self.clave, "Github", "user1")
+        
+        with self.assertRaises(ErrorCorreoNoEstablecido):
+            self.gestor.enviar_correo2fa(self.clave, "Github", "user1")
+
+
+    @patch("gestor_credenciales.gestor_credenciales.smtplib.SMTP")
+    def test_codigo_sin_establecer(self, mock_smtp):
+
+        correo = "prueba@gmail.com"
+        self.gestor.establecer_correo(self.clave, "Github", "user1", correo)
+
+        with self.assertRaises(ErrorCodigoNoEstablecido):
+            self.gestor.enviar_correo2fa(self.clave, "Github", "user1")
+
+
+    def test_verificacion2fa_codigo_sin_crear(self):
+        with self.assertRaises(ErrorCodigoNoEstablecido):
+            self.gestor.verificar_usuario(self.clave, "Github", "user1", "123456")
+
+    def test_verificacion2fa(self):
+
+        correo = "prueba@gmail.com"
+        self.gestor.establecer_correo(self.clave, "Github", "user1", correo)
+        self.gestor.crear_codigo2fa(self.clave, "Github", "user1")
+
+        # Verifica que se resta un intento a cada llamada erronea
+        self.gestor.verificar_usuario(self.clave, "Github", "user1", "1")
+        self.assertEqual(self.gestor.obtener_intentos_restantes(self.clave, "Github", "user1"), 2)
+        self.gestor.verificar_usuario(self.clave, "Github", "user1", "1")
+        self.assertEqual(self.gestor.obtener_intentos_restantes(self.clave, "Github", "user1"), 1)
+        self.gestor.verificar_usuario(self.clave, "Github", "user1", "1")
+        self.assertEqual(self.gestor.obtener_intentos_restantes(self.clave, "Github", "user1"), 0)
+
+    def test_verificacion2fa_sin_intentos(self):
+
+        correo = "prueba@gmail.com"
+        self.gestor.establecer_correo(self.clave, "Github", "user1", correo)
+        self.gestor.crear_codigo2fa(self.clave, "Github", "user1")
+
+        self.gestor.verificar_usuario(self.clave, "Github", "user1", "1")
+        self.gestor.verificar_usuario(self.clave, "Github", "user1", "1")
+        self.gestor.verificar_usuario(self.clave, "Github", "user1", "1")
+
+        with self.assertRaises(ErrorSinIntentosRestantes):
+            self.gestor.verificar_usuario(self.clave, "Github", "user1", "1")
+
+    def test_verificacion2fa_usuario_verificado(self):
+
+        correo = "prueba@gmail.com"
+        self.gestor.establecer_correo(self.clave, "Github", "user1", correo)
+        self.gestor.crear_codigo2fa(self.clave, "Github", "user1")
+
+        self.gestor.verificar_usuario(self.clave, "Github", "user1", self.gestor.obtener_codigo2fa(self.clave, "Github", "user1"))
+
+        # Comprueba que el usuario se ha verificado con la llamada anterior
+        self.assertTrue(self.gestor.comprobacion_verificacion_usuario(self.clave, "Github", "user1"))
+
+        # Comprueba que el codigo_auth es None tras la verificacion
+        self.assertIsNone(self.gestor.obtener_codigo2fa(self.clave, "Github", "user1"))
+
+        # Comprueba que salta la excepcion al intentar verificar al usuario que ya esta verificado
+        with self.assertRaises(ErrorUsuarioYaVerificado):
+            self.gestor.verificar_usuario(self.clave, "Github", "user1", "1")
+
+
+if __name__ == "__main__":
+    unittest.main()
