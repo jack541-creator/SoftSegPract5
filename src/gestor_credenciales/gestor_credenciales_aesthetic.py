01d3dcf4c75e7fbcfce9da6a1eb2ad882f43ec66
_______ <anoniimo7373+github@gmail.com>
2026-04-28 19:35:19 +0200

    Incrustar Función Añadir Credenciales
    Hemos creado una copia del código original para ir incrustando las funciones

--- src/gestor_credenciales/gestor_credenciales_aesthetic.py
+++ src/gestor_credenciales/gestor_credenciales_aesthetic.py
@@ -5,14 +5,14 @@ import bcrypt
 import smtplib
 from icontract import require, ensure
 # ---- clases para errores (excepciones)  ----------------------------------------------------------------------
-class ErrorPoliticaPassword(Exception):pass
-class ErrorAutenticacion(Exception):pass
-class ErrorServicioNoEncontrado(Exception):pass
-class ErrorCredencialExistente(Exception):pass
-class ErrorCorreoNoEstablecido(Exception):pass
-class ErrorCodigoNoEstablecido(Exception):pass
+class ErrorPoliticaPassword(Exception): pass
+class ErrorAutenticacion(Exception): pass
+class ErrorServicioNoEncontrado(Exception): pass
+class ErrorCredencialExistente(Exception): pass
+class ErrorCorreoNoEstablecido(Exception): pass
+class ErrorCodigoNoEstablecido(Exception): pass
 class ErrorSinIntentosRestantes(Exception):pass
-class ErrorUsuarioYaVerificado(Exception):pass
+class ErrorUsuarioYaVerificado(Exception): pass
 #---- clase principal -------------------------------------------------------------------------------
 class GestorCredenciales:
     def __init__(self, clave_maestra: str):
@@ -44,9 +44,38 @@ class GestorCredenciales:
     @ensure(lambda servicio, usuario, result: result is None)
     
     def añadir_credencial(self, clave_maestra: str, servicio: str, usuario: str, password: str) -> None:
+        simbolos = "!>;'\/$#[]{}:\n\r"
+        palabras_peligrosas = ["DROP", "DELETE", "UPDATE", "ALTER", "CREATE", "TABLE", "ALERT", "SCRIPT", "EXECUTE", "IMMEDIATE"]
+        
+        if not isinstance(usuario, str):
+            raise TypeError
+        if usuario.replace('.', '', 1).replace('-', '', 1).isdigit():
+            raise ValueError
+        
+        usuario = usuario.strip()
+        password = password.strip()
+        servicio = servicio.strip()
+        clave_maestra = clave_maestra.strip()
+    
+        if not self._verificar_clave(clave_maestra, self._clave_maestra_hashed):
+            raise PermissionError                                                   # ErrorAutenticacion
+    
+        if not servicio or not usuario or (len(usuario) > 255):
+            raise ValueError
+    
+        if any(c in simbolos for c in usuario) or any(c in simbolos for c in servicio) or any(p.upper() in palabras_peligrosas for p in servicio.split()):
+            raise ValueError
+    
+        # voy a asumir que verificar_fortaleza_password() es llamado antes de esta función
         if servicio not in self._credenciales:
             self._credenciales[servicio] = {}
-        self._credenciales[servicio][usuario] = password
+        
+        if usuario in self._credenciales[servicio]:
+            p = self._credenciales[servicio][usuario]
+            if bcrypt.checkpw(password.encode('utf-8'), p)::
+                raise ValueError       # ErrorCredencialExistente
+        
+        self._credenciales[servicio][usuario] = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())  
         
     # obtener password ----------------------------------------------------------------------------
     @require(lambda clave_maestra: isinstance(clave_maestra, str) and len(clave_maestra.strip()) > 0)
