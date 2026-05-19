be3942b73850af209029e261aa1c4e321475590e
Ismael <ismaesco06@gmail.com>
2026-05-20 01:54:45 +0200

    Versión 0 errores con OTPs

--- src/gestor_credenciales/gestor_credenciales.py
+++ src/gestor_credenciales/gestor_credenciales.py
@@ -1,4 +1,4 @@
-import bcrypt
+import bcrypt, string, random
 from abc import ABC, abstractmethod
 
 # =========================================================
@@ -281,9 +281,9 @@ class GestorCredenciales:
             raise ErrorCredencialExistente()
 
         self._autenticar(clave_maestra) # Mediación completa
-        self._credenciales[servicio][usuario] = bcrypt.hashpw(
+        self._credenciales[servicio][usuario] = {"hash" : bcrypt.hashpw(
             password.encode("utf-8"), bcrypt.gensalt()
-        )
+        )}
 
         return True
 
@@ -302,7 +302,7 @@ class GestorCredenciales:
         ):
             raise ErrorServicioNoEncontrado()
 
-        return self._credenciales[servicio][usuario]
+        return self._credenciales[servicio][usuario]["hash"]
     
     # =====================================================
     # es_password_segura (wrapper de verificar_fortaleza)
@@ -417,5 +417,68 @@ class GestorCredenciales:
 
         self._autenticar(clave_maestra) # Mediación Completa
         self._credenciales[servicio][usuario_nuevo] = password_hashed
+
+        return True
+
+    # =====================================================
+    # OTPs
+    # =====================================================
+
+    def generar_otps(self, cantidad: int) -> list:
+
+        if not isinstance(cantidad, int):
+            raise TypeError
+
+        if cantidad <= 0: 
+            raise ValueError
+
+
+        caracteres = string.ascii_letters + string.digits
+
+    # SET = no permite duplicados
+        otps = set()
+
+        while len(otps) < cantidad:
+
+            otp = ''.join(random.choice(caracteres) for _ in range(6))
+
+            otps.add(otp)
+
+        return list(otps)
+
+    def almacenar_otps(self, clave_maestra: str, servicio: str, usuario: str, otps: list) -> None:
+        """
+        Almacena las OTPs del usuario.
+        Reemplaza las anteriores si existían.
+        """
+
+    # Validación básica
+        if not isinstance(otps, list):
+            raise TypeError
+
+    # Guardar OTPs
+        # Guardar COPIA independiente
+        self._credenciales[servicio][usuario]["otps"] = otps.copy()
+
+
+    def verificar_otp(self, clave_maestra, servicio, usuario, otp):
+
+        print("ENTRANDO EN verificar_otp")
+
+        if not isinstance(otp, str):
+            return False
+
+        if len(otp) != 6:
+            return False
+
+        if otp in self._credenciales[servicio][usuario]["otps"]:
+
+            print("ELIMINANDO OTP")
+
+            self._credenciales[servicio][usuario]["otps"].remove(otp)
+
+            return True
+
+        return False
 
         return True
