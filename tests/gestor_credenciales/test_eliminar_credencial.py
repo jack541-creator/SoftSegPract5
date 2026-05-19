7192c417ef8da7bdc7d723bb759f8ec7aa33f4b4
Jack Walter <jackrolandwalter@gmail.com>
2026-05-19 18:36:52 +0200

    Sacar los tests de test_funcionales_gestor_credenciales.py en archivos separados -Jack

--- tests/gestor_credenciales/test_eliminar_credencial.py
+++ tests/gestor_credenciales/test_eliminar_credencial.py
@@ -2,70 +2,74 @@ import unittest
 from src.gestor_credenciales.gestor_credenciales import GestorCredenciales, ErrorPoliticaPassword, ErrorAutenticacion
 import bcrypt
 
-def test_eliminar_credencial_existente(self):
-    clave = "claveMaestraSegura123!"
-    self.gestor.anadir_credencial(clave, "GitHub", "user1", "PasswordSegura123!")
+class TestEliminiarCredencial(unittest.TestCase):
 
-    resultado = self.gestor.eliminar_credencial(clave, "GitHub", "user1")
+    def setUp(self):
+        self.clave = "claveMaestraSegura123!"
+        self.gestor = GestorCredenciales(clave_maestra=self.clave)
+        
 
-    self.assertTrue(resultado)
+    def test_eliminar_credencial_existente(self):
+        self.gestor.anadir_credencial(self.clave, "GitHub", "user1", "PasswordSegura123!")
 
-    with self.assertRaises(Exception):
-        self.gestor.obtener_password(clave, "GitHub", "user1")
+        resultado = self.gestor.eliminar_credencial(self.clave, "GitHub", "user1")
 
+        self.assertTrue(resultado)
 
-def test_eliminar_credencial_inexistente(self):
-    clave = "claveMaestraSegura123!"
-
-    with self.assertRaises(Exception):
-        self.gestor.eliminar_credencial(clave, "GitLab", "user1")
-
-
-def test_eliminar_credencial_clave_incorrecta(self):
-    clave = "claveMaestraSegura123!"
-
-    self.gestor.anadir_credencial(clave, "GitHub", "user1", "PasswordSegura123!")
-
-    with self.assertRaises(ErrorAutenticacion):
-        self.gestor.eliminar_credencial("abc", "GitHub", "user1")
-
-
-def test_eliminar_credencial_valores_invalidos(self):
-    clave_valida = "claveMaestraSegura123!"
-
-    casos_invalidos = [
-        ("GitHub", ""),
-        ("GitHub", None),
-        (123, "user1"),
-        ("GitHub", 456),
-    ]
-
-    for servicio, usuario in casos_invalidos:
         with self.assertRaises(Exception):
-            self.gestor.eliminar_credencial(clave_valida, servicio, usuario)
+            self.gestor.obtener_password(self.clave, "GitHub", "user1")
 
-    claves_invalidas = [
-        "",
-        None,
-        123,
-    ]
 
-    for clave in claves_invalidas:
+    def test_eliminar_credencial_inexistente(self):
         with self.assertRaises(Exception):
-            self.gestor.eliminar_credencial(clave, "GitHub", "user1")
+            self.gestor.eliminar_credencial(self.clave, "GitLab", "user1")
 
 
-def test_eliminar_credencial_usuario_no_existente(self):
-    clave = "claveMaestraSegura123!"
-    self.gestor.anadir_credencial(clave, "GitHub", "user1", "PasswordSegura123!")
+    def test_eliminar_credencial_clave_incorrecta(self):
+        self.gestor.anadir_credencial(self.clave, "GitHub", "user1", "PasswordSegura123!")
 
-    with self.assertRaises(Exception):
-        self.gestor.eliminar_credencial(clave, "GitHub", "user2")
+        with self.assertRaises(ErrorAutenticacion):
+            self.gestor.eliminar_credencial("abc", "GitHub", "user1")
 
-    password_guardada = self.gestor.obtener_password(clave, "GitHub", "user1")
 
-    self.assertIsInstance(password_guardada, bytes)
-    self.assertNotEqual(password_guardada, b"PasswordSegura123!")
-    self.assertTrue(
-        bcrypt.checkpw("PasswordSegura123!".encode("utf-8"), password_guardada)
-    )
+    def test_eliminar_credencial_valores_invalidos(self):
+        clave_valida = "claveMaestraSegura123!"
+
+        casos_invalidos = [
+            ("GitHub", ""),
+            ("GitHub", None),
+            (123, "user1"),
+            ("GitHub", 456),
+        ]
+
+        for servicio, usuario in casos_invalidos:
+            with self.assertRaises(Exception):
+                self.gestor.eliminar_credencial(clave_valida, servicio, usuario)
+
+        claves_invalidas = [
+            "",
+            None,
+            123,
+        ]
+
+        for clave in claves_invalidas:
+            with self.assertRaises(Exception):
+                self.gestor.eliminar_credencial(clave, "GitHub", "user1")
+
+
+    def test_eliminar_credencial_usuario_no_existente(self):
+        self.gestor.anadir_credencial(self.clave, "GitHub", "user1", "PasswordSegura123!")
+
+        with self.assertRaises(Exception):
+            self.gestor.eliminar_credencial(self.clave, "GitHub", "user2")
+
+        password_guardada = self.gestor.obtener_password(self.clave, "GitHub", "user1")
+
+        self.assertIsInstance(password_guardada, bytes)
+        self.assertNotEqual(password_guardada, b"PasswordSegura123!")
+        self.assertTrue(
+            bcrypt.checkpw("PasswordSegura123!".encode("utf-8"), password_guardada)
+        )
+
+if __name__ == "__main__":
+    unittest.main()
