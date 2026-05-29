dfa0985a9b4c6b430591dd3e1ac28f799962342e
_______ <anoniimo7373+github@gmail.com>
2026-05-29 09:50:33 +0200

    arreglo el error ( era literalmente el nombre º_º )

--- tests/ciphercoin/test_modelo.py
+++ tests/ciphercoin/test_modelo.py
@@ -533,7 +533,7 @@ class TestAutenticacion(unittest.TestCase):
 import tkinter as tk
 from unittest.mock import MagicMock, patch, PropertyMock
 from src.ciphercoin.modelo import TipoWallet
-from gui import DashboardFrame, VentanaTransferencia, VentanaHistorial
+from src.ciphercoin.gui import DashboardFrame, VentanaTransferencia, VentanaHistorial
 
 # helpers de mock
 
