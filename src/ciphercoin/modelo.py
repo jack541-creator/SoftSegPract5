4c7f9488fd801f44221a54d493158075a11835c3
sgwb1dn33 <holagithub@gmail.com>
2026-06-02 20:24:26 +0200

    Update modelo.py

--- src/ciphercoin/modelo.py
+++ src/ciphercoin/modelo.py
@@ -10,7 +10,7 @@ from enum import Enum
 from typing import Optional
 import icontract
 from src.logger.access_control import ContextoSeguridad, RolUsuario
-from logger.log_util import (anadir_al_log, verificar_cadena_hashes, 
+from src.logger.log_util import (anadir_al_log, verificar_cadena_hashes, 
     inicializar_log, configure_logging, existe_archivo, archivo_vacio)
 
 from src.gestor_credenciales.gestor_credenciales import (GestorCredenciales, ErrorAutenticacion)
